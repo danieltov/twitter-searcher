27f2035ba0f41b04dca181558fcf2ff85a7d5189
Daniel Tovar <daniel@danieltovar.io>
2019-08-28 01:26:47 -0600

    Link to original tweets

--- src/Tweets.js
+++ src/Tweets.js
@@ -7,7 +7,13 @@ import './Tweets.css'
 
 const mapTweets = tweets => {
   return tweets.map(tweet => (
-    <Card key={tweet.id} className='my-3'>
+    <Card
+      key={tweet.id}
+      className='my-3 text-decoration-none text-white bg-info'
+      as='a'
+      href={`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`}
+      target='_blank'
+      rel='noopener noreferrer'>
       <Card.Body>
         <Card.Text>{tweet.text}</Card.Text>
       </Card.Body>
