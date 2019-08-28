439a358a187b118a4914c82847a0813627ca0e2e
Daniel Tovar <daniel@danieltovar.io>
2019-08-27 20:37:08 -0600

    brought in bootstrap

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import { getTweets, getUser } from './api';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   useEffect(() => {
-    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-    const url =
-      'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=realdonaldtrump';
-    fetch(proxyurl + url, {
-      method: 'GET',
-      headers: {
-        Authorization:
-          'Bearer AAAAAAAAAAAAAAAAAAAAACcR%2FwAAAAAAa%2BLTPME0kHq1aHBrSmYo8AJmdEw%3DUObh6gRSOoRzqEA1WCys2DzfgZO4NwHnSt78lIS2sf3AxwAmIo'
-      }
-    })
-      .then(res => res.json())
-      .then(result => console.log(result), error => console.error(error));
+    getTweets('realDonaldTrump');
+    getUser('realDonaldTrump');
   });
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
