f02b3c98bd4de3626a14c54b5e6638c8f6a4cb54
Daniel Tovar <daniel@danieltovar.io>
2019-08-28 00:45:47 -0600

    Conditional rendering logic for User card

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import useGlobal from './store'
 
 import Container from 'react-bootstrap/Container'
 import Search from './Search'
+import User from './User'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [globalState, globalActions] = useGlobal()
+
+  const { status } = globalState
 
   useEffect(() => {
     globalActions.getData('realDonaldTrump')
@@ -18,6 +21,15 @@ function App() {
     <Container className='App'>
       <h1 className='text-center'>Twitter Searcher</h1>
       <Search />
+      {status === 'ERROR' ? (
+        'There was an error, try again.'
+      ) : status === 'NOT_FOUND' ? (
+        'The user was not found.'
+      ) : status === 'SUCCESS' ? (
+        <User />
+      ) : (
+        'Loading...'
+      )}
     </Container>
   )
 }
