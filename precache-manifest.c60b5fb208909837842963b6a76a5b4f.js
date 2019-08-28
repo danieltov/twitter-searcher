3d8eb3abd998f157c2a9ac1395b5940432f096d3
Daniel Tovar <daniel@danieltovar.io>
2019-08-27 19:42:28 -0600

    Updates

---
+++ precache-manifest.c60b5fb208909837842963b6a76a5b4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e4c112aa6e191373b841d8c5990c81",
+    "url": "/twitter-searcher/index.html"
+  },
+  {
+    "revision": "2a12a634549e7c29e2f7",
+    "url": "/twitter-searcher/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5490b6817145806cb8fa",
+    "url": "/twitter-searcher/static/js/2.691a397e.chunk.js"
+  },
+  {
+    "revision": "2a12a634549e7c29e2f7",
+    "url": "/twitter-searcher/static/js/main.9b34d78e.chunk.js"
+  },
+  {
+    "revision": "843c70ebcf670402b207",
+    "url": "/twitter-searcher/static/js/runtime~main.6fb7cb48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twitter-searcher/static/media/logo.5d5d9eef.svg"
+  }
+]);
